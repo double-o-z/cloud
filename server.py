d1d0858be22b4ad975e2380f55a297e1f545f04c
Or Ohev-Zion <or.ohev.zion@gmail.com>
2016-09-01 19:05:07 +0300

    created server and client socket scripts.

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+from mpmath import mp
+
+DIGITS = 300000
+
+
+class ThreadedServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(5)
+        print "Listening to clients."
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target=self.listen_to_client, args=(client, address)).start()
+
+    def listen_to_client(self, client, address):
+        while True:
+            try:
+                data = client.recv(1024)
+                if data:
+                    response = self.calc_pie_digits()
+                    client.send(response)
+                    print "Sent a client: {}, pie's first {} digits.".format(address, DIGITS)
+                    client.close()
+                else:
+                    raise socket.error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+    def calc_pie_digits(self):
+        mp.dps = DIGITS
+        return str(mp.pi)
+
+if __name__ == "__main__":
+    ThreadedServer('', 8080).listen()
